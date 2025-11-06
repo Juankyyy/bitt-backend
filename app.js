8c1305639ed9101305fa6b28d9ccf85571bc564e
Juankyyy <juancamiloherrera960@gmail.com>
2025-11-06 17:44:19 -0500

    add: cookie-parser, dotenv, helmet, and nodemon
    Added 'cookie-parser', 'dotenv', and 'helmet' to dependencies for improved security and environment variable management. Added 'nodemon' to devDependencies for automatic server restarts during development.

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import 'dotenv/config';
+
+import { corsMiddleware } from './middlewares/cors.js';
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(helmet());
+app.use(corsMiddleware());
+
+
+const PORT = process.env.PORT || 1111;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+})
