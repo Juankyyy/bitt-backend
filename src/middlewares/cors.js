eb927069c08254d910d505788bed41828b7e0077
Juankyyy <juancamiloherrera960@gmail.com>
2025-11-15 15:34:48 -0500

    feat(cors): origins

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 export const corsMiddleware = () =>
   cors({
     origin: (origin, callback) => {
-      const ACCEPTED_ORIGINS = [process.env.CLIENT_URL, process.env.FRONT_URL];
+      const ACCEPTED_ORIGINS = [process.env.PROD_FRONT, process.env.LOCAL_FRONT];
 
       if (ACCEPTED_ORIGINS.includes(origin)) {
         return callback(null, true);
